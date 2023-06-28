7f440aa41d2733909096cfca396bab51b28ba628
Isaac Stofko <isaacstofko@gmail.com>
2023-06-28 17:52:25 -0400

    footer position

--- client/src/components/pages/Footer.js
+++ client/src/components/pages/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+const styles = {
+  footerStyle: {
+    display: 'flex',
+    justifyContent: 'center',
+    position: 'fixed',
+    bottom: 0,
+  },
+};
+
 function Footer({currentPage, handlePageChange}) {
     return(
-  <footer className="footer">
+  <footer className="footer" style={styles.footerStyle}>
     <div >
         FOOTER
   </div>
