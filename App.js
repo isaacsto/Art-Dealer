4c2de5cb85529d7f222751653589d67a753ffa03
Nestor <you@example.com>
2023-06-29 20:44:25 -0400

    added files

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from "./src/Componets/header.navabar";
+
+function App() {
+    return (
+        <React.Fragment>
+            <Navbar/>
+        </React.Fragment>
+    );
+}
+
+export default App;
