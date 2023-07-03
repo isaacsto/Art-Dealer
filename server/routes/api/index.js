35960383e2cbafeee6aab4bc45681d397e420637
frankolms <frankolms@gmail.com>
2023-07-03 19:39:40 -0400

    added artistRoutes to index.js in api folder

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const userRoutes = require("./user-routes");
+const artistRoutes = require("./artist-routes");
 
 router.use("/users", userRoutes);
+router.use("/artists", artistRoutes);
 
 module.exports = router;
