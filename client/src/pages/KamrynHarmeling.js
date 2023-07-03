379c27e478c69d44a69a681e5c49e46443c67d78
Basil-NB <120618274+Basil-NB@users.noreply.github.com>
2023-07-03 17:27:42 -0400

    added a row function to the artists pages with a description

--- client/src/pages/KamrynHarmeling.js
+++ client/src/pages/KamrynHarmeling.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import DisplayCard from './DisplayCard'
+import '../Styles/ArtistsPages.css'
 
 function KamrynHarmeling() {
+   const artistName = 'Kamryn Harmeling';
+   const artistDescription = 'This is a brief description of the artist Kamryn Harmeling.';
+   const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+   const createRows = (cardsPerRow) => {
+      const rows = [];
+      let row = [];
+
+      cards.forEach((card, index) => {
+         row.push(<DisplayCard key={index} />);
+
+         if ((index + 1) % cardsPerRow === 0 || index === cards.length - 1) {
+            rows.push(
+               <div className='art-cards-row' key={index}>
+                  {row}
+               </div>
+            );
+            row = [];
+         }
+      });
+
+      return rows;
+   };
+
    return (
-
-          <card>
-              <DisplayCard />
-          </card>
-      
-  )
-
+      <div className='art-cards-container'>
+         <div className='artist-info'>
+            <h2>{artistName}</h2>
+            <p>{artistDescription}</p>
+         </div>
+         {createRows(3)}
+      </div>
+   );
 }
 
 export default KamrynHarmeling; 
