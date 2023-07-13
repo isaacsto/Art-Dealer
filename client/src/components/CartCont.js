23fcad03f4450239b4bf46499cb9d5661b481e6c
Isaac Stofko <isaacstofko@gmail.com>
2023-07-12 21:28:55 -0400

    got dropdown to work in cart

--- client/src/components/CartCont.js
+++ client/src/components/CartCont.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Cart from '../pages/Cart'
 
+const style = {
+    height: '200px'
+}
+
 function CartCont() {
     return (
         <div>
+            <div style={style}>
+
+            </div>
 <Cart/>
         </div>
     )
