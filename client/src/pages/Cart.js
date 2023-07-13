0c437650a597cf7880e744ae229acf3ecd65ca8a
Cashew1337 <121908566+Cashew1337@users.noreply.github.com>
2023-07-12 20:59:37 -0400

    edit env variables

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -9,7 +9,7 @@ import { useStoreContext } from '../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../utils/actions';
 // import './style.css';
 
-const stripePromise = loadStripe(`${process.env.PK_STRIPE}`);
+const stripePromise = loadStripe(`${process.env.REACT_APP_PK_STRIPE}`);
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
