ac21064d0c21e071a298016b82413aeb28a71a97
Isaac Stofko <isaacstofko@gmail.com>
2024-04-12 10:50:07 -0400

    hp buttons functionality

--- client/src/components/MediumButtons.js
+++ client/src/components/MediumButtons.js
@@ -6,17 +6,20 @@ function MediumButtons() {
 
             <div className="med-wrap">
             <div class="med-label"> <h2>Explore Different Mediums</h2></div>
-            <div className="item">
+            <div className="item"><a href="/PaintingPage">
                 <div class="text-overlay one">Painting</div>
                 <img src={require("../assets/bluish.jpg")} alt="blue painting by Isabella Saracini"/>
+                </a>
             </div>
-            <div className="item">
+            <div className="item"><a href="/SculptingPage">
             <div class="text-overlay two">Sculpture</div>
             <img src={require("../assets/marineclay.jpg")} alt="sculpture by Aaron Dubois" />
+            </a>
             </div>
-            <div className="item">
+            <div className="item"><a href="/DigitalArtPages">
             <div class="text-overlay three">Digital Art</div>
             <img src={require("../assets/reeds.jpg")} alt="digital art sketch by Nicolas Riley"/>
+            </a>
             </div>
             </div>
     
